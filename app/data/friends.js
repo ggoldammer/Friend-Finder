fe8b29fa2002af4788ec5c3b1069257e7a6e72e4
Gabriel Viveros-Goldammer <gabrielgoldammer@gmail.com>
2018-11-04 22:17:40 -0700

    Added Routes

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+var friends = [
+    {
+        "name":"Captain America",
+        "photo": "../public/img/captainamerica.jpeg",
+        "scores":[
+            1,
+            5,
+            3,
+            4,
+            5,
+            2,
+            1,
+            3,
+            1,
+            1
+          ]
+      },
+      {
+        "name":"Black Widow",
+        "photo": "../public/img/blackwidow.jpg",
+        "scores":[
+            4,
+            2,
+            1,
+            5,
+            3,
+            4,
+            4,
+            2,
+            5,
+            2
+          ]
+      },
+
+      {
+        "name":"Thor",
+        "photo":"../public/img/thor.jpg",
+        "scores":[
+            5,
+            3,
+            5,
+            1,
+            3,
+            1,
+            3,
+            5,
+            4,
+            5
+          ]
+      },
+
+      {
+        "name":"Hulk",
+        "photo":"../public/img/hulk.jpg",
+        "scores":[
+            5,
+            3,
+            5,
+            1,
+            5,
+            1,
+            1,
+            1,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"IronMan",
+        "photo":"../public/img/ironman.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            5,
+            3,
+            4,
+            2,
+            3,
+            4
+          ]
+      },      
+] 
+  module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,18 +1,13 @@
-const server = require("../../server");
+var path = require("path");
 
-const app = express();
-const port = process.env.PORT || 8080;
+module.exports = function(app) {
 
-module.exports = function (app) {
-    app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "./app/public/survey.html"));
-    });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "./app/public/home.html"));
-    });
+  app.get("/survey.html", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
 
-    app.get("*", function (req, res){
-        res.sendFile(path.join(__dirname, "./app/public/home.html"));
-    });
-}
+};
