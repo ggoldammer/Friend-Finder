72bdb7b1279d15409c7282b5236e26dc12b79b60
unknown <gaboguy@gmail.com>
2019-01-22 19:54:15 -0700

    Added friends and index.html

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+var friends = [
+    {
+        "name":"Captain America",
+        "photo": "../public/img/captainamerica.jpeg",
+        "scores":[
+            1,
+            1,
+            3,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Black Widow",
+        "photo":"../public/img/blackwidow.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            2,
+            5,
+            1,
+            4,
+            5,
+            1,
+            1
+          ]
+      },
+
+      {
+        "name":"Thor",
+        "photo":"../public/img/thor.jpg",
+        "scores":[
+            1,
+            1,
+            4,
+            3,
+            5,
+            3,
+            2,
+            4,
+            4,
+            2
+          ]
+      },
+
+      {
+        "name":"Hulk",
+        "photo":"../public/img/hulk.jpg",
+        "scores":[
+            3,
+            1,
+            2,
+            4,
+            2,
+            1,
+            1,
+            5,
+            4,
+            2
+          ]
+      },
+
+      {
+        "name":"IronMan",
+        "photo":"../public/img/ironman.jpg",
+        "scores":[
+            3,
+            1,
+            2,
+            2,
+            2,
+            1,
+            1,
+            1,
+            1,
+            5
+          ]
+      },      
+] 
+  module.exports = friends;
